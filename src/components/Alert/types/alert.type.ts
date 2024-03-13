be93d40db4903ba6df25f083f1c7d32c0a9f6756
Archan Ray <archan.ray2011@gmail.com>
2024-03-13 11:04:42 +0530

    read me update

--- src/components/Alert/types/alert.type.ts
+++ src/components/Alert/types/alert.type.ts
@@ -1,6 +1,13 @@
 import React from "react";
+import { AssetProps } from "../../../icons/types/asset.types";
 
-export interface AlertProps extends React.SVGProps<SVGSVGElement> {
-  size?: number;
-  color?: string;
+export interface AlertProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+  type?: "flat" | "outlined" | "default";
+  status?: "success" | "warning" | "error";
+  showIcon?: boolean;
+  icon?: AssetProps;
+  showCloseIcon?: boolean;
+  onClose?: () => void;
+  title?: string;
+  description?: string;
 }
