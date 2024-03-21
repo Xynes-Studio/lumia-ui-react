92ce7514a65ca8fe6188ea47e145f147bf711963
Archan Ray <archan.ray2011@gmail.com>
2024-03-21 15:33:05 +0530

    all text editable

--- src/texts/Text/Text.tsx
+++ src/texts/Text/Text.tsx
@@ -4,12 +4,15 @@ import { color, typography } from "../../shared/styles";
 import styled from "styled-components";
 import "../texts.styles.css";
 import { TextProps } from "./text.types";
-import './code-highlighting.css';
+import "./code-highlighting.css";
 
 const TextComponent: React.ForwardRefRenderFunction<
   HTMLParagraphElement,
   TextProps
-> = ({ children, type = "body", textCase = "none", ...props }, ref) => {
+> = (
+  { children, type = "body", textCase = "none", editable = false, ...props },
+  ref
+) => {
   const StyleText = styled.p`
     font-family: ${type === "body"
       ? typography.type.primary
@@ -31,15 +34,25 @@ const TextComponent: React.ForwardRefRenderFunction<
       ? color.foreground
       : type === "caption"
       ? color.foreground
-      : color.foreground};;
+      : color.foreground};
     line-height: 150%;
     text-transform: ${textCase};
   `;
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLHeadingElement>) => {
+    // Prevent line breaks on Enter key
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+    const value = (event.target as HTMLHeadingElement).textContent;
+    console.log("Value:", value);
+  };
   return (
     <StyleText
       ref={ref}
       className={cx("lmTextComponent", props.className)}
+      contentEditable={editable}
+      onKeyDown={handleKeyDown}
       {...props}
     >
       {children}
