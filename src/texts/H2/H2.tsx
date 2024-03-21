14b03af921fbe19aa50b23f77b07c7d93125a310
Archan Ray <archan.ray2011@gmail.com>
2024-03-21 15:30:33 +0530

    all texts editable

--- src/texts/H2/H2.tsx
+++ src/texts/H2/H2.tsx
@@ -2,13 +2,16 @@ import React, { forwardRef } from "react";
 import { cx } from "../../utils";
 import { typography } from "../../shared/styles";
 import styled from "styled-components";
-import '../texts.styles.css';
+import "../texts.styles.css";
 import { HeaderProps } from "@texts/text.typoes";
 
 const H2Component: React.ForwardRefRenderFunction<
   HTMLHeadingElement,
   HeaderProps
-> = ({ children, type = 'serif', numberOfLines = 0, ...props }, ref) => {
+> = (
+  { children, type = "serif", numberOfLines = 0, editable = false, ...props },
+  ref
+) => {
   const StyleH2 = styled.h2`
     font-family: ${type === "serif"
       ? typography.type.title
@@ -21,10 +24,22 @@ const H2Component: React.ForwardRefRenderFunction<
       line-clamp: ${numberOfLines};
     `}
   `;
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLHeadingElement>) => {
+    // Prevent line breaks on Enter key
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+    const value = (event.target as HTMLHeadingElement).textContent;
+    console.log("Value:", value);
+  };
+
   return (
     <StyleH2
       ref={ref}
       className={cx("lmTextComponent", props.className)}
+      contentEditable={editable}
+      onKeyDown={handleKeyDown}
       {...props}
     >
       {children}
