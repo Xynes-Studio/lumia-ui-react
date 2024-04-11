6988b689d90bef2bf750c3c8ecc45aaf9938e5ff
sanketmukherjee97 <sanketmukherjee189official@gmail.com>
2024-04-11 23:33:16 +0530

    button modified

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -6,7 +6,7 @@ import { cx } from "@utils/cx";
 import { LMAsset } from "@utils/LumiaAssetManager";
 import { Text } from "@texts/Text/Text";
 import { color as globalColor } from "@shared/styles";
-import { spacing } from "@shared/styles";
+import { spacing, strokes } from "@shared/styles";
 import "./button.styles.css";
 
 const ButtonComponent: React.ForwardRefRenderFunction<
@@ -20,7 +20,7 @@ const ButtonComponent: React.ForwardRefRenderFunction<
     color = globalColor.foreground,
     iconAtEnd = false,
     backgroundColor,
-    borderColor = globalColor?.border,
+    borderColor = globalColor?.border1,
     borderRadius = spacing?.borderRadius?.small,
     ...props
   },
@@ -28,8 +28,10 @@ const ButtonComponent: React.ForwardRefRenderFunction<
 ) => {
   const ButtonContainer = styled.button`
     background-color: ${type !== "label" ? backgroundColor : "none"};
-    border: ${type !== "outlined" ? "none" : `0.3vw solid ${borderColor} `};
-    padding: calc(${spacing?.padding?.small} / 4) ${spacing?.padding?.small};
+    border: ${type !== "outlined"
+      ? "none"
+      : `${strokes?.s0} solid ${globalColor?.border1}`};
+    padding: calc(${spacing?.padding?.small} / 2) ${spacing?.padding?.medium};
     border-radius: ${borderRadius};
     flex-direction: ${iconAtEnd ? "row-reverse" : "row"};
   `;
@@ -37,7 +39,7 @@ const ButtonComponent: React.ForwardRefRenderFunction<
   return (
     <ButtonContainer className={cx("lmButtonContainer")} ref={ref} {...props}>
       <LMAsset
-        style={iconAtEnd ? { marginLeft: "0.5vw" } : { marginRight: "0.5vw" }}
+        style={iconAtEnd ? { marginLeft: "0.8vw" } : { marginRight: "0.8vw" }}
         visible={icon !== undefined}
         Asset={icon}
         color={color}
