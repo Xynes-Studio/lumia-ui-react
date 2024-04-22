a3fffe95e9a010775e427a935db4b3f6ebb41d70
sanketmukherjee97 <sanketmukherjee189official@gmail.com>
2024-04-22 22:10:47 +0530

    Search Input updated

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,6 @@ export * from "./Switch/switch";
 export * from "./Checkbox/checkbox";
 export * from "./Code/code";
 export * from "./Inputs/TextInput/textInput";
+export * from "./Inputs/SearchInput/searchInput";
 export * from "./Button/button";
 export * from "./Disclosuers/Tabs/tabs";
