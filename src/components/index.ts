4f39efd68c6db853ffd387399d4414cbc62142c5
sanketmukherjee97 <sanketmukherjee189official@gmail.com>
2024-04-25 17:42:58 +0530

    updated accordion

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,4 @@ export * from "./Inputs/TextInput/textInput";
 export * from "./Inputs/SearchInput/searchInput";
 export * from "./Button/button";
 export * from "./Disclosuers/Tabs/tabs";
+export * from "./Disclosuers/Accordion/accordion";
