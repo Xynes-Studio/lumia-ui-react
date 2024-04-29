91d218e37de7072152b39c3d0f7d0f313bce095a
Aman Kazi <aman@xynes.com>
2024-04-29 17:18:57 +0530

    mdx prser completed

--- src/utils/StringToJsx/types.d.ts
+++ src/utils/StringToJsx/types.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as Babel from '@types/babel__standalone';
 import * as React from '@types/react';
 import { TransformOptions } from "@babel/core";
@@ -5,6 +7,7 @@ import React, { FunctionComponent, FC, PropsWithChildren } from 'react';
 import { TransformOptions } from "@babel/core";
 export interface IStringToReactApi {
     updateTemplate: (template: string, babelOptions: TransformOptions) => IStringToReactApi;
+    // eslint-disable-next-line @typescript-eslint/ban-types
     getComponent: () => FC<PropsWithChildren<{}>>;
     [x: string]: any;
 }
