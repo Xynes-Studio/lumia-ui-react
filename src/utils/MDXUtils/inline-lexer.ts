0358c105e519e450a73e831b35375110e3eab532
Aman Kazi <aman@xynes.com>
2024-05-04 10:26:49 +0530

    lexers

--- src/utils/MDXUtils/inline-lexer.ts
+++ src/utils/MDXUtils/inline-lexer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /**
  * @license
  *
@@ -7,7 +8,8 @@
  * Copyright (c) 2018-2021, Костя Третяк. (MIT Licensed)
  * https://github.com/ts-stack/markdown
  */
-
+/* eslint-disable */
+// @ts-nocheck 
 import { ExtendRegexp } from './extend-regexp';
 import {
   Link,
@@ -26,24 +28,24 @@ import { Renderer } from './renderer';
  * Inline Lexer & Compiler.
  */
 export class InlineLexer {
-  protected static rulesBase: RulesInlineBase = null;
+  protected static rulesBase: RulesInlineBase;
   /**
    * Pedantic Inline Grammar.
    */
-  protected static rulesPedantic: RulesInlinePedantic = null;
+  protected static rulesPedantic: RulesInlinePedantic;
   /**
    * GFM Inline Grammar
    */
-  protected static rulesGfm: RulesInlineGfm = null;
+  protected static rulesGfm: RulesInlineGfm;
   /**
    * GFM + Line Breaks Inline Grammar.
    */
-  protected static rulesBreaks: RulesInlineBreaks = null;
-  protected rules: RulesInlineBase | RulesInlinePedantic | RulesInlineGfm | RulesInlineBreaks;
+  protected static rulesBreaks: RulesInlineBreaks;
+  protected rules: RulesInlineBase | RulesInlinePedantic | RulesInlineGfm | RulesInlineBreaks | undefined;
   protected renderer: Renderer;
-  protected inLink: boolean;
-  protected hasRulesGfm: boolean;
-  protected ruleCallbacks: RulesInlineCallback[];
+  protected inLink: boolean = false;
+  protected hasRulesGfm: boolean = false;
+  protected ruleCallbacks: RulesInlineCallback[] = [];
 
   constructor(
     protected staticThis: typeof InlineLexer,
@@ -172,34 +174,34 @@ export class InlineLexer {
    * Lexing/Compiling.
    */
   output(nextPart: string): string {
-    let execArr: RegExpExecArray;
+    let execArr: RegExpExecArray | null | undefined;
     let out = '';
 
     while (nextPart) {
       // escape
-      if ((execArr = this.rules.escape.exec(nextPart))) {
+      if ((execArr = this.rules?.escape.exec(nextPart))) {
         nextPart = nextPart.substring(execArr[0].length);
         out += execArr[1];
         continue;
       }
 
       // autolink
-      if ((execArr = this.rules.autolink.exec(nextPart))) {
-        let text: string;
-        let href: string;
+      if ((execArr = this.rules?.autolink.exec(nextPart))) {
+        let text: string | undefined;
+        let href: string | undefined;
         nextPart = nextPart.substring(execArr[0].length);
 
         if (execArr[2] === '@') {
-          text = this.options.escape(
+          text = this.options?.escape && this.options?.escape(
             execArr[1].charAt(6) === ':' ? this.mangle(execArr[1].substring(7)) : this.mangle(execArr[1]),
           );
           href = this.mangle('mailto:') + text;
         } else {
-          text = this.options.escape(execArr[1]);
+          text =  this.options?.escape && this.options.escape(execArr[1]);
           href = text;
         }
-
-        out += this.renderer.link(href, null, text);
+        if(href)
+          out += this.renderer.link(href, null, text);
         continue;
       }
 
