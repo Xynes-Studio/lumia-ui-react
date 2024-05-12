f194e7221eebe14651e23fac141bee7dc0272632
Aman Kazi <aman@xynes.com>
2024-05-12 11:57:46 +0530

    validations

---
+++ src/components/Inputs/SearchInput/searchInput.style.ts
@@ -0,0 +1,25 @@
+import { Flex } from "@app/View";
+import { color, shadow, spacing, strokes, typography } from "@shared/styles";
+import styled from "styled-components";
+
+export const SearchInputContainer = styled(Flex)`
+  position: relative;
+`;
+export const SearchInputWrapper = styled(Flex)<{type:string}>`
+  background-color: ${props=>props.type === "default"
+    ? color?.foregroundInverse400
+    : "none"};
+  border: ${props=>props.type === "outline"
+    ? `${strokes?.s0} solid ${color?.border1}`
+    : "null"};
+  padding: ${spacing?.padding?.p0} ${spacing?.padding?.p2};
+  border-radius: ${spacing?.borderRadius?.r0};
+  box-shadow: ${props=>props.type === "shadow" ? shadow : "none"};
+`;
+export const SearchInputStyle = styled.input`
+  font-size: ${typography?.size?.input};
+`;
+
+export const SearchResultsWrapper = styled(SearchInputWrapper)`
+  top: 18vh;
+`;
