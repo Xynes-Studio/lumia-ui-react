359ed5e35fb41a150a2f8458d37bf610ef00b277
Archan Ray <archan.ray2011@gmail.com>
2024-06-09 10:02:49 +0530

    all theme updated

--- src/View/DataTable/datatable.styles.ts
+++ src/View/DataTable/datatable.styles.ts
@@ -1,24 +1,22 @@
 import styled from "styled-components";
 import { Flex } from "@app/View";
-import { shadow, spacing } from "@shared/styles";
 import { Text } from "@texts/index";
-import { color } from "@shared/styles";
 
 export const DataTableContainer = styled(Flex)`
-  padding: ${spacing.padding.p3};
+  padding: ${({ theme }) => theme.spacing.padding.p3};
 `;
 
 export const TableWithoutSearchContainer = styled(Flex)`
   overflow: hidden;
   height: fit-content;
-  border-radius: ${spacing.borderRadius.r1};
-  box-shadow: ${shadow};
+  border-radius: ${({ theme }) => theme.spacing.borderRadius.r1};
+  box-shadow: ${({ theme }) => theme.shadow};
 `;
 
 export const HeaderContainer = styled(Flex)`
   flex-direction: row;
-  margin-bottom: ${spacing.padding.p2};
-  background-color: ${color.foregroundInverse300};
+  margin-bottom: ${({ theme }) => theme.spacing.padding.p2};
+  background-color: ${({ theme }) => theme.color.foregroundInverse300};
   justify-content: center;
   align-items: center;
 `;
@@ -30,14 +28,14 @@ export const RowContainer = styled(Flex)`
 export const FooterContainer = styled(Flex)`
   flex-direction: row;
   justify-content: center;
-  margin-top: ${spacing.padding.p2};
+  margin-top: ${({ theme }) => theme.spacing.padding.p2};
 `;
 
 export const SearchContainer = styled.div<{
   searchBarPosition: "start" | "center" | "end";
 }>`
   width: 30%;
-  margin-bottom: ${spacing.padding.p3};
+  margin-bottom: ${({ theme }) => theme.spacing.padding.p3};
   align-self: ${({ searchBarPosition }) =>
     searchBarPosition === "center"
       ? "center"
--- src/View/Lists/BulletNumberedList/BulletNumberedList.type.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export interface BulletNumberedListProps extends React.HtmlHTMLAttributes<HTMLOListElement | HTMLUListElement> {
-  listType?: "numbered-list" | "bullet-points";
-  children?: React.ReactNode;
-}
-
